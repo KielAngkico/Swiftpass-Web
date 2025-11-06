c865313e6637834eb17d1219130d8373f4585c7e
KielAngkico <KielAngkico@gmail.com>
2025-11-07 02:40:22 +0800

    Update

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 const http = require("http");
 const app = require("./app");
-const { setupWebSocket } = require("../webSocket");
+const { setupWebSocket } = require("../webSocket/websocket");
 
 const HOST = process.env.HOST || "localhost";
 const PORT = process.env.PORT || 5000;

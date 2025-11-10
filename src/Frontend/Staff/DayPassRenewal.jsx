1901b751268db7183393f5a0ca889165c0683f15
KielAngkico <KielAngkico@gmail.com>
2025-11-10 13:15:07 +0800

    Update

--- src/Frontend/Staff/DayPassRenewal.jsx
+++ src/Frontend/Staff/DayPassRenewal.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import api from "../../api";
-import { useToast } from "../../../components/ToastManager";
+import { useToast } from "../../components/ToastManager";
 
 function formatDateToLocalString(date) {
   const yyyy = date.getFullYear();

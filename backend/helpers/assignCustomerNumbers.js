ce8ef2137aa6fa668a790619105d498249d55ce5
KielAngkico <aerykangkico@gmail.com>
2026-05-14 02:27:58 +0800

    Updates

--- backend/helpers/assignCustomerNumbers.js
+++ backend/helpers/assignCustomerNumbers.js
@@ -16,8 +16,9 @@ const [[{ existingCount }]] = await conn.query(`
   FROM RegisteredRfid
   WHERE allocated_to_admin = ?
     AND role = ?
-    AND status = 'in_use'
+    AND status != 'replaced'
     AND id != ?
+    AND customer_number IS NOT NULL
 `, [adminId, role, rfidIds[i]]);
 
     console.log(`   Existing count (excluding id ${rfidIds[i]}): ${existingCount}`);
